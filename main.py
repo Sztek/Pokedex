b70172d1cf2b7ee5a179f50827c3ae3d3344b6e6
Sztek <jakubrogusz@gmail.com>
2022-10-19 09:41:09 +0200

    Sterowanie

--- main.py
+++ main.py
@@ -13,7 +13,38 @@ class Pole:
 
 class Postac:
     def __init__(self):
-        self.body = Rect(4*64+8, 4*64, 48, 64)
+        self.x = 4
+        self.y = 4
+        self.dx = 0
+        self.dy = 0
+        self.timer = 0
+        self.body = Rect(self.x*64, self.y*64, 64, 64)
+        self.texture = image.load('ele.png')
+
+    def tick(self):
+        self.body = Rect(self.x * 64, self.y * 64, 64, 64)
+        if self.timer:
+            self.timer -= 1
+            self.x += self.dx
+            self.y += self.dy
+            if not self.timer:
+                self.x = round(self.x)
+                self.y = round(self.y)
+        else:
+            self.dx = 0
+            self.dy = 0
+
+    def move(self, kierunek, kolizja):
+        if not self.timer and not self.dx and not self.dy and not kolizja:
+            self.timer = 10
+            if kierunek == 0:
+                self.dy = -0.1
+            elif kierunek == 1:
+                self.dy = 0.1
+            elif kierunek == 2:
+                self.dx = -0.1
+            elif kierunek == 3:
+                self.dx = 0.1
 
 
 pygame.init()
@@ -27,8 +58,9 @@ for i in range(12):
     for j in range(9):
         plansza[i].append(Pole())
         plansza[i][j].set(i, j)
+        if i == 0 or j == 0 or i == 8 or j == 8:
+            plansza[i][j].kolizja = True
 
-print(plansza[0])
 screen = display.set_mode(size)
 
 while 1:
@@ -36,11 +68,20 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    if pygame.key.get_pressed()[K_w]:
+        hero.move(0, plansza[int(hero.x)][int(hero.y)-1].kolizja)
+    elif pygame.key.get_pressed()[K_s]:
+        hero.move(1, plansza[int(hero.x)][int(hero.y)+1].kolizja)
+    elif pygame.key.get_pressed()[K_a]:
+        hero.move(2, plansza[int(hero.x)-1][int(hero.y)].kolizja)
+    elif pygame.key.get_pressed()[K_d]:
+        hero.move(3, plansza[int(hero.x)+1][int(hero.y)].kolizja)
 
+    hero.tick()
     screen.fill(black)
     for i in range(9):
         for j in range(9):
             draw.rect(screen, Color(50+j*20, 150, 50+i*20), plansza[i][j].body)
-    draw.rect(screen, Color(255, 255, 255), hero.body)
+    screen.blit(hero.texture, hero.body)
     screen.blit(hud, (576, 0, 192, 768))
     display.flip()
