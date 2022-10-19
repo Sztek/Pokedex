ed5ebe3562b3ba09424005cbe1532edc56ea35b5
Sztek <jakubrogusz@gmail.com>
2022-10-19 11:31:55 +0200

    Teren

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import sys, pygame, random
 from pygame import *
 
 
@@ -6,9 +6,20 @@ class Pole:
     def __init__(self):
         self.kolizja = False
         self.body = Rect(0, 0, 64, 64)
+        self.texture = 0
 
-    def set(self, x, y):
-        self.body = Rect(i*64, j*64, 64, 64)
+    def set(self, x, y, tile):
+        sizex = (64, 64)
+        self.texture = Surface(sizex)
+        img = image.load('Tile-SpringGround.png')
+        img = transform.scale(img, (160*4, 112*4))
+        img.scroll(64, 64)
+        if tile == 0:
+            self.texture.blit(img, (0, 0), (512, 128, 64, 64))
+        else:
+            self.texture.blit(img, (0, 0), (128, 128, 64, 64))
+        self.body = Rect(x * 64, y * 64, 64, 64)
+        self.texture.scroll(64, 64)
 
 
 class Postac:
@@ -36,15 +47,15 @@ class Postac:
 
     def move(self, kierunek, kolizja):
         if not self.timer and not self.dx and not self.dy and not kolizja:
-            self.timer = 10
+            self.timer = 20
             if kierunek == 0:
-                self.dy = -0.1
+                self.dy = -0.05
             elif kierunek == 1:
-                self.dy = 0.1
+                self.dy = 0.05
             elif kierunek == 2:
-                self.dx = -0.1
+                self.dx = -0.05
             elif kierunek == 3:
-                self.dx = 0.1
+                self.dx = 0.05
 
 
 pygame.init()
@@ -53,18 +64,19 @@ black = 0, 0, 0
 hero = Postac()
 plansza = []
 hud = image.load('hud.png')
-for i in range(12):
+
+for i in range(9):
     plansza.append([])
     for j in range(9):
         plansza[i].append(Pole())
-        plansza[i][j].set(i, j)
+        plansza[i][j].set(i, j, random.randrange(7))
         if i == 0 or j == 0 or i == 8 or j == 8:
             plansza[i][j].kolizja = True
 
 screen = display.set_mode(size)
 
 while 1:
-    pygame.time.Clock().tick(30)
+    pygame.time.Clock().tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -81,7 +93,8 @@ while 1:
     screen.fill(black)
     for i in range(9):
         for j in range(9):
-            draw.rect(screen, Color(50+j*20, 150, 50+i*20), plansza[i][j].body)
+            #draw.rect(screen, Color(50+j*20, 150, 50+i*20), plansza[i][j].body)
+            screen.blit(plansza[i][j].texture, plansza[i][j].body)
     screen.blit(hero.texture, hero.body)
     screen.blit(hud, (576, 0, 192, 768))
     display.flip()
